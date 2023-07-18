816e90c8b443587f2bdace1ee8c24b1d6f76e84a
deadManAlive <alfarrakhan@gmail.com>
2023-07-19 00:05:35 +0700

    realign with c++11

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using comptime::IsPrime;
 using comptime::Fibonacci;
 
 int main() {
-    constexpr auto z = 1746987019;
+    const auto z = 51131;
     std::cout << z << " " << (IsPrime<z>::value ? "is a prime." : "is not a prime.") << std::endl;
 
     constexpr size_t y = 15;
--- src/const_isprime.h
+++ src/const_isprime.h
@@ -1,45 +1,47 @@
 #ifndef CONST_ISPRIME_H_
 #define CONST_ISPRIME_H_
 
-#define INT_TY_ASRT static_assert(std::is_integral<decltype(N)>::value, "only accept integral types");
+#include <cstdint>
+
+#define INT_TY_ASRT static_assert(std::is_integral<size_t>::value, "only accept integral types");
 
 namespace comptime {
-    template<auto N, decltype(N) I, bool X = (I * I <= N), bool E = (N % I == 0 || N % (I + 2) == 0)>
+    template<size_t N, size_t I, bool X = (I * I <= N), bool E = (N % I == 0 || N % (I + 2) == 0)>
     struct IsPrimeLoop {
         INT_TY_ASRT
     };
 
-    template<auto N, decltype(N) I>
+    template<size_t N, size_t I>
     struct IsPrimeLoop<N, I, true, false> {
         INT_TY_ASRT
         STC(bool) value = IsPrimeLoop<N, I + 6>::value;
     };
 
-    template<auto N, decltype(N) I, bool E>
+    template<size_t N, size_t I, bool E>
     struct IsPrimeLoop<N, I, false, E> {
         INT_TY_ASRT
         STC(bool) value = true;
     };
 
-    template<auto N, decltype(N) I>
+    template<size_t N, size_t I>
     struct IsPrimeLoop<N, I, true, true> {
         INT_TY_ASRT
         STC(bool) value = false;
     };
 
-    template<auto N, bool X = (N == 2 || N == 3), bool Y = (N <= 1 || N % 2 == 0 || N % 3 == 0)>
+    template<size_t N, bool X = (N == 2 || N == 3), bool Y = (N <= 1 || N % 2 == 0 || N % 3 == 0)>
     struct IsPrime {
         INT_TY_ASRT
         STC(bool) value = IsPrimeLoop<N, 5>::value;
     };
 
-    template <auto N, bool Y>
+    template <size_t N, bool Y>
     struct IsPrime<N, true, Y> {
         INT_TY_ASRT
         STC(bool) value = true;
     };
 
-    template <auto N, bool X>
+    template <size_t N, bool X>
     struct IsPrime<N, X, true> {
         INT_TY_ASRT
         STC(bool) value = false;
